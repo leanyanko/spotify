b403df1078eaedbecb6a3de8d375d970be182fd2
meinou <mskozlovskaya@gmail.com>
2018-06-14 12:40:41 -0400

    final

--- src/com/anna/DecodeString.java
+++ src/com/anna/DecodeString.java
@@ -17,55 +17,77 @@ import java.util.Arrays;
 
 public class DecodeString {
 
-    public static String ans;
-
-    public static String decoder(String s) {
-        ans = "";
-        for (int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            if (ch > '0' && ch < '9') repeater(Character.getNumericValue(ch), s, i + 2);
-            else if( ch == '[' || ch == ']') {}
-
-            else {
-                ans += Character.toString(ch);
-                System.out.println("ans " + ans);
-            }
-
-        }
-   //     repeater(s.length(), s, 0);
-        return ans;
+    static class Pointer {
+        int val;
     }
 
+    int pointer;
 
-    private static void repeater (int amount, String s, int start){
-        char ch = s.charAt(start);
-        System.out.println("amount " + amount);
-        System.out.println(ch + " from " + start);
+    public static String decodeString(String s) {
 
-        for (int i = 0; i < amount; i ++) {
-            int j = start;
-            System.out.println("j = start = " + j);
+        int len = s.length();
+        Pointer pointer = new Pointer();
+        pointer.val = 0;
 
-            while(ch != ']') {
-                System.out.println(ch + "="+ j);
-                j++;
+        StringBuilder sb = new StringBuilder();
 
-                if (ch > '0' && ch < '9') {
-                    int numeric = Character.getNumericValue(ch);
-                    System.out.println("numeric " + numeric);
-
-                    repeater(numeric, s, j + 1);
-                }
-                else if(ch == '[') j++;
-                else {
-                    ans += Character.toString(ch);
-                    System.out.println("ans " + ans);
-                }
-                ch = s.charAt(j);
+        while(pointer.val < len) {
+            char c = s.charAt(pointer.val++);
+            if (c >= '0' && c <= '9') {
+                sb.append(parseCount(c, pointer, s));
+            } else {
+                sb.append(c);
             }
         }
+        return sb.toString();
     }
 
+    private static String parseCount(char c, Pointer pointer, String s) {
+        StringBuilder numBuilder = new StringBuilder();
+        numBuilder.append(c);
+
+        while(pointer.val < s.length()) {
+            char next = s.charAt(pointer.val++);
+            if (next >= '0' && next <= '9') {
+                numBuilder.append(next);
+            } else {
+                pointer.val--;
+                break;
+            }
+        }
+        int count = Integer.valueOf(numBuilder.toString());
+        return(multiply(count, getTextBetweenBrackets(pointer, s)));
+    }
+
+    private static String getTextBetweenBrackets(Pointer pointer, String s) {
+        StringBuilder sb = new StringBuilder();
+
+        while(pointer.val < s.length()) {
+            char next = s.charAt(pointer.val++);
+
+            if (next == '[') {    
+                continue;
+            } else if (next == ']') {  
+                return sb.toString();
+
+            } else {     
+                if (next >= '0' && next <= '9') {
+                    sb.append(parseCount(next, pointer, s));
+                } else {
+                    sb.append(next);
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    private static String multiply(int count, String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < count; i++) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
     public static void main(String[] args) {
         System.out.println(decoder("2[b3[a]]"));
     }
